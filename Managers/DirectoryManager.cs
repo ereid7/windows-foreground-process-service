a702ce5bf9dd3ef32f2208c2d10f68c73cb6718d
ereid7 <evan@reids.us>
2020-09-13 19:51:27 -0700

    Create directory manager

---
+++ Managers/DirectoryManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace AppTimerService.Managers
+{
+    public class DirectoryManager
+    {
+
+        private readonly string _appTimerDataPath;
+
+        public DirectoryManager()
+        {
+            var programDataPath = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData);
+            this._appTimerDataPath = $"{programDataPath}\\AppTimer";
+
+            this.initializeDirectoryManager();
+        }
+
+        private void initializeDirectoryManager()
+        {
+            this.verifyProgramDataDirectory();
+            this.verifyDailyDataDirectory();
+        }
+
+        // TODO add error handling
+        private void verifyProgramDataDirectory()
+        {
+            if (!Directory.Exists(this._appTimerDataPath))
+            {
+                Directory.CreateDirectory(this._appTimerDataPath);
+            }
+        }
+        
+        private void verifyDailyDataDirectory()
+        {
+            var dailyDataPath = $"{this._appTimerDataPath}\\{DateTime.Today.ToString("dd-MM-yyyy")}";
+            if (!Directory.Exists(dailyDataPath))
+            {
+                Directory.CreateDirectory(dailyDataPath);
+            }
+        }
+    }
+}
--- Worker.cs
+++ Worker.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using AppTimerService.Managers;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -13,45 +15,46 @@ namespace AppTimerService
     {
         private readonly ILogger<Worker> _logger;
         private readonly HashSet<int> _processIds;
+        private readonly DirectoryManager _directoryManager;
 
         public Worker(ILogger<Worker> logger)
         {
             _logger = logger;
             _processIds = new HashSet<int>();
+            _directoryManager = new DirectoryManager();
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             while (!stoppingToken.IsCancellationRequested)
             {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now); 
+                //_logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
                 Process[] processes = Process.GetProcesses();
-
                 foreach (Process p in processes)
                 {
                     if (!String.IsNullOrEmpty(p.MainWindowTitle))
                     {
+                        //_logger.LogInformation(p.MainWindowTitle);
+                        //_logger.LogInformation(p.StartTime.ToString());
+                        //_logger.LogInformation(p.Id.ToString());
+
                         if (!_processIds.Contains(p.Id))
                         {
-                            _logger.LogInformation(p.MainWindowTitle);
-                            _logger.LogInformation(p.StartTime.ToString());
-                            _logger.LogInformation(p.Id.ToString());
                             _processIds.Add(p.Id);
-      
+
+                            _logger.LogInformation($"Process {p.MainWindowTitle} started at {p.StartTime}");
+
+                            p.EnableRaisingEvents = true;
+                            p.Exited += (s, e) =>
+                            {
+                                _processIds.Remove(p.Id);
+                                _logger.LogInformation($"Process {p.MainWindowTitle} stopped at {p.ExitTime}");
+                            };
                         }
                     }
                 }
 
-                foreach (int p in _processIds.ToList())
-                {
-                    var process = Process.GetProcessById(p);
-                    var timeOpen = DateTime.Now.Subtract(process.StartTime);
-                    _logger.LogInformation($"{process.MainWindowTitle}: {timeOpen.Duration()}");
-                }
-
-
-
-                await Task.Delay(10000, stoppingToken);
+                await Task.Delay(1000, stoppingToken);
             }
         }
     }
